eb268d886bfa18be5a2e16d634465b2d941da358
Akmal Rian <akmalrian@gmail.com>
2026-06-02 17:52:14 +0700

    feat : add api.js

---
+++ src/services/api.js
@@ -0,0 +1,30 @@
+
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:9000";
+
+const apiFetch = async (endpoint, options = {}) => {
+    const url = `${BASE_URL}${endpoint}`;
+    const defaultHeaders = {
+        "Content-Type": "aplication/json",
+    };
+    const token = localStorage.getItem("token");
+    if (token) {
+        defaultHeaders["Authorization"] = `Bearer ${token}`;
+    }
+    const config = {
+        ...options,
+        headers: {
+            ...defaultHeaders,
+            ...options.headers,
+        },
+    };
+
+    const response = await fetch(url, config);
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.error || data.message || "Something went wrong");
+    }
+    return data;
+};
+
+export default apiFetch;
